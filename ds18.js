fc3c3454a3cb60b0255730fa26b9a721fbe3e82b
Hannes Prirschl <hannes.prirschl@me.com>
2015-08-09 13:21:26 +0200

    code cleanup

--- ds18.js
+++ ds18.js
@@ -1,7 +1,7 @@
 /**
  * Created by pari on 06.08.15.
  *
- * ds18 reads the temperature on a raspberry pi using the ds18b20 sensor and stores the data in a mongodb.
+ * ds18 reads the temperature on a raspberry pi using the ds18b20 sensor and stores the data in a dynamoDB.
  * the sensor information can be read from the /sys/bus/w1/devices/28-<serialnr>/w1_slave file.
  */
 
@@ -18,14 +18,13 @@ var db = new AWS.DynamoDB({signatureVersion: 'v4', region: 'eu-central-1'});
 var readTemperature = new Promise(function (resolve, reject) {
         fs.readFile(config.path + '/' + config.file, {encoding:'utf8'}, function(err, data) {
             "use strict";
-            if(err) reject(err);
+            if(err) {console.log(err.message()); reject(err); }
             // we expect YES at the end of the first line
             if(data.search("YES") > 1) {
-                console.log("readTemp");
                 resolve({ //unix timestamp + 1/1000 degree celsius
                     "date": parseInt(Math.round(Date.now()/1000),10).toString(),
                     // for generating dummy-data: "celsius": (parseInt(data.split("t=")[1], 10) + parseInt(Math.random() * 2000))
-                    "celsius": data.split("t=")[1]
+                    "celsius": data.split("t=")[1].trim()
                 });
             }
         });
@@ -33,10 +32,10 @@ var readTemperature = new Promise(function (resolve, reject) {
 
 
 
-readTemperature.then(function(err,data) {
+readTemperature.then(function(data) {
     "use strict";
-    if(err) reject(err);
-    console.log("DEBUG: date/celsius: " + data.date + "/" + data.celsius);
+    if(!data) { throw ("could not fetch temperature data");;}
+    //console.log("DEBUG: date/celsius: " + data.date + "/" + data.celsius);
     var dbparams = {
         Item: {
             timestamp: {S: data.date },
@@ -44,12 +43,12 @@ readTemperature.then(function(err,data) {
         },
         TableName: "rpi-temp"
     };
+
+    console.log(dbparams);
     db.putItem(dbparams, function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else     console.log(data);           // successful response
+        if (err) console.log(err, err.stack);    // an error occurred
+        u// else     console.log(data);           // successful response
     });
-
-
 }).catch(function(e) {
     "use strict";
     console.log("Error: " + e.message());
